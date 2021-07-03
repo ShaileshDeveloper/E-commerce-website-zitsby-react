99874b249d2b573959284039a1806e299b2e1fb9
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2021-07-03 20:58:55 +0530

    initial commit
    created react-app in the repo

---
+++ sports-e-commerce/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXVc5GNbQdVaxHtDIraURVWSFrVOG9D3g",
+    authDomain: "zitsby-e-commerce.firebaseapp.com",
+    projectId: "zitsby-e-commerce",
+    storageBucket: "zitsby-e-commerce.appspot.com",
+    messagingSenderId: "110953884515",
+    appId: "1:110953884515:web:5d44f3a0e2e60c5bacc156",
+    measurementId: "G-3G18CJT57P"
+  };
