d4f332066ca6576919ccf7c26bc9454ff09a3081
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2021-07-14 17:33:11 +0530

    feat -updated counter button

--- sports-e-commerce/src/Homepage/productsection.js
+++ sports-e-commerce/src/Homepage/productsection.js
@@ -2,40 +2,75 @@ import React from "react";
 import "./productsection.css";
 import data from "../Data/datahomepage";
 import { Usecart } from "../cartpage/cartcontext";
-
-
+import { checkItem } from "../util";
+import { NavLink } from "react-router-dom";
 
 export function Card(item) {
-    const { dispatch } = Usecart();
-  
-    return (
-      <div className="product">
-        <img src={item.image} alt="ss" />
-        <span style={{ color: "red" }}>{item.product}</span>
-        <span style={{ color: "gray" }}>${item.price}</span>
-        <div className="button__container">
-          <button onClick={() => dispatch({ type: "ADD_TO_CART", item })}>
+  const { state, dispatch } = Usecart();
+
+  return (
+    <div className="product">
+      <img src={item.image} alt="ss" />
+      <span
+        style={{
+          color: "red",
+        }}
+      >
+        {item.product}
+      </span>
+      <span
+        style={{
+          color: "gray",
+        }}
+      >
+        $ {item.price}
+      </span>
+      <div className="button__container">
+
+
+        {
+          checkItem(state.cart, item.id) ? (
+          <NavLink to="/cart">
+            <button> Go to cart </button>
+            </NavLink>
+        ) : (
+          <button
+            onClick={() =>
+              dispatch({
+                type: "ADD_TO_CART",
+                item,
+              })
+            }
+          >
             AddToCart
           </button>
-          <button onClick={() => dispatch({ type: "ADD_TO_WISHLIST" , item})}>Wishlist</button>
-        </div>
+        )}
+
+
+
+        <button
+          onClick={() =>
+            dispatch({
+              type: "ADD_TO_WISHLIST",
+              item,
+            })
+          }
+        >
+          Wishlist
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 function Productsection() {
-
   return (
     <div className="product__section__container">
       {data.map((item) => {
-        return (
-         <Card {...item} />
-        );
+        return <Card {...item} />;
       })}
     </div>
   );
 }
 
 export default Productsection;
-
-
---
+++ sports-e-commerce/src/util.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+
+// function finder(cart , item ){
+//     return state.cart.dfindrr
+// }
+
+// export default util
+
+export const checkItem = (array, id) => {
+    return array.find((item) => item.id === id);
+  };
